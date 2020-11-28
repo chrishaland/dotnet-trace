b094d84ca24199f7c7ae8f98a88fbe995134385a
Chris Håland <chris.haland@outlook.com>
2020-11-28 23:35:59 +0100

    Test ensuring B3 headers are not created

--- Tests.Http/B3_traces_should_not_be_set_when_request_headers_does_not_exist_test.cs
+++ Tests.Http/B3_traces_should_not_be_set_when_request_headers_does_not_exist_test.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Tests.Http
@@ -9,8 +12,24 @@ namespace Tests.Http
         [Test]
         public async Task B3_traces_should_not_be_set_when_request_headers_does_not_exist()
         {
-            await Task.CompletedTask;
-            Assert.Pass();
+            var response = await TestHost.SUT.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            using var contentStream = new MemoryStream(Encoding.UTF8.GetBytes(content));
+            var traces = await JsonSerializer.DeserializeAsync<TraceMetadataResponse>(contentStream, TestHost.SerializerOptions);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(traces.TraceId, Is.Null);
+                Assert.That(traces.SpanId, Is.Null);
+                Assert.That(traces.ParentSpanId, Is.Null);
+                Assert.That(traces.ParentSpanId, Is.Null);
+                Assert.That(traces.Sampled, Is.Null);
+                Assert.That(traces.Flags, Is.Null);
+                Assert.That(traces.B3, Is.Null);
+            });
         }
     }
 }
